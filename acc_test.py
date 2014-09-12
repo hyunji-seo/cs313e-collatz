623d3c5e33995cd1d4105f2aecc239a3649857aa
hyunji-seo <hyunji-seo@users.noreply.github.com>
2014-09-11 21:54:34 -0500

    Delete acc_test.py

--- acc_test.py
+++
@@ -1,9 +0,0 @@
-# 1000 acceptance tests
-import random
-# generate random number pairs
-for n in range(1000):
-    i = random.randint(1, 100000)
-    j = random.randint(1, 100000)
-    if i != j:
-    	print (str(i) + " " + str(j))
-
